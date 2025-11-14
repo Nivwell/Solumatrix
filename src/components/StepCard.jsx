a1091e5a9e958c97d60aa9afdafce2e588e787b4
Nivwell <nivllew@gmail.com>
2025-11-14 01:33:43 -0600

    Step

--- src/components/StepCard.jsx
+++ src/components/StepCard.jsx
@@ -1,4 +1,4 @@
-import '../styles/StepCard.css';
+import '../styles/stepCard.css';
 import { BlockMath, InlineMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 import { toFrac } from '../utils/formatFraction';
